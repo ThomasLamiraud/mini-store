da4be261f2bddd0980a440feff171c2e0a11922d
Thomas Lamiraud <Thomas.lamiraud@gmail.com>
2015-08-19 11:22:08 +0200

    add brand logic

--- app/controllers/admin/brands_controller.rb
+++ app/controllers/admin/brands_controller.rb
@@ -6,5 +6,24 @@ module Admin
     def index
       @brands = Brand.all
     end
+
+    def create
+      @brand = Brand.new(brand_params)
+      if @brand.save
+        redirect_to admin_brands_path, alert: 'Nouvelle marque ajoutée'
+      else
+        return render :new
+      end
+    end
+
+    def new
+      @brand = Brand.new
+    end
+
+    private
+
+    def brand_params
+      params.require(:brand).permit(:name)
+    end
   end
 end
