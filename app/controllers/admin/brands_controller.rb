d152d1208f2578d2575be6035ddb85e306518deb
Thomas Lamiraud <Thomas.lamiraud@gmail.com>
2015-08-19 13:09:59 +0200

    Edit and Delete brand logic

--- app/controllers/admin/brands_controller.rb
+++ app/controllers/admin/brands_controller.rb
@@ -1,5 +1,6 @@
 module Admin
   class BrandsController < AdminController
+    before_action :set_brand, only: [:edit, :destroy, :update]
     before_action :authenticate_user!
     before_action :admin_only
 
@@ -20,10 +21,35 @@ module Admin
       @brand = Brand.new
     end
 
+    def edit
+
+    end
+
+    def update
+      respond_to do |format|
+        if @brand.update(brand_params)
+          format.html { redirect_to admin_brands_path, alert: 'Votre marque a été mis à jour avec succès !' }
+          format.json { render :show, status: :ok, location: @brand }
+        else
+          format.html { render :edit }
+          format.json { render json: @brand.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+
+    def destroy
+      @brand.destroy
+      redirect_to admin_brands_path, alert: "Suppression effectuée avec succès !"
+    end
+
     private
 
     def brand_params
       params.require(:brand).permit(:name)
+    end
+
+    def set_brand
+      @brand = Brand.find(params[:id])
     end
   end
 end
