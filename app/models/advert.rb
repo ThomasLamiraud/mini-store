1a45a06a7df40f3a216f3755dd9a72dfddf93931
Thomas Lamiraud <Thomas.lamiraud@gmail.com>
2015-08-26 10:57:08 +0200

    add file field edit advert

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -2,7 +2,7 @@ class Advert < ActiveRecord::Base
 	has_one :order
 	belongs_to :model
 
-	has_attached_file :picture, styles: { top: "800x500", default: "700x400" }, default_url: "/images/:style/missing.png"
+	has_attached_file :picture, styles: { top: "800x500#", default: "700x400#" }, default_url: "/images/:style/missing.png"
 
 	validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
 end
