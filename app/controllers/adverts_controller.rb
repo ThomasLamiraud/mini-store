20a24050da3f2f979fb434bcfb399c6c938853cf
Thomas Lamiraud <Thomas.lamiraud@gmail.com>
2015-08-26 12:45:25 +0200

    add link to advert show

--- app/controllers/adverts_controller.rb
+++ app/controllers/adverts_controller.rb
@@ -1,7 +1,19 @@
 class AdvertsController < ApplicationController
+  before_action :set_advert, only: [:show]
 
   def index
     @adverts = Advert.order(created_at: :desc)
     @adverts_top4 = Advert.order(created_at: :desc).limit(4)
   end
+
+  def show
+
+  end
+
+  private
+
+  def set_advert
+    @advert = Advert.find(params[:id])
+  end
+
 end
